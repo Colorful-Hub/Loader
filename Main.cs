ac2e7daf03e104c2c098dcd898980d2f2d20d342
NightsTimeZ <84789294+NightsTimeZ@users.noreply.github.com>
2024-03-06 22:37:00 +0700

    Update and rename README.md to Main.cs

---
+++ Main.cs
@@ -0,0 +1,4 @@
+print("This Game Id is : "..game.gameId)
+local e = request or http_request
+local a = e()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Colorful-Hub/Royx_PC/main/loader.lua"))();
