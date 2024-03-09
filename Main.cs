7b782b9fd5822e877592805773950187f4a4dbcb
NightsTimeZ <84789294+NightsTimeZ@users.noreply.github.com>
2024-03-10 02:12:22 +0700

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,4 +1,11 @@
 print("This Game Id is : "..game.gameId)
 local e = request or http_request
-local a = e()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Colorful-Hub/Royx_PC/main/loader.lua"))();
+local a = e({
+    Url = "https://raw.githubusercontent.com/Colorful-Hub/ColorfulHub/main/"..game.gameId..".lua",
+    Method = "GET"
+})
+if a.StatusCode == 200 then
+    loadstring(a.Body)()
+else
+    game.Players.LocalPlayer:Kick("\n Not Have This Game")
+end
